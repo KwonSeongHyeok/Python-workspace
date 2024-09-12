1c6529b5d1ff52bb1b5e7a1a64b8dd76a20794b2
KwonSeongHyeok <aiqksiwk@naver.com>
2024-09-12 11:44:26 +0900

    python

---
+++ chap1_print.py
@@ -0,0 +1,13 @@
+'''
+Java에서 출력할 때 메인 메서드 안에 출력 메서드 작성
+public static void main(String args[]){
+    System.out.println("안녕하세요.");
+}
+'''
+
+print('안녕하세요')
+
+#변수 출력하기
+name = "kh학생"
+age = 20
+print(f"{name} 이고, {age} 살 입니다.")
